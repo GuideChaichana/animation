b142bff7452242ac22d576944a3459f55e1c6ed0
GuideChaichana <s40602@bj.ac.th>
2024-11-22 16:31:47 +0700

    Add files via upload

---
+++ a2_direction_12/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1732267895,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/collide-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/playeranimation-sheet0.png",
+		"images/playeranimation-sheet1.png",
+		"images/wall.png",
+		"images/bg-sheet0.png",
+		"images/bgsheet-sheet0.png",
+		"images/download-sheet0.png",
+		"images/screenshot-sheet0.png",
+		"images/ฟไกไฟ-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
