ad27bc6498cde0bc39105a8932121d222a8bb431
GuideChaichana <s40602@bj.ac.th>
2025-02-03 15:27:13 +0700

    Add files via upload

---
+++ a5_flappybird_12/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1738571273,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/tiledbackground.png",
+		"images/pipbottom.png",
+		"images/piptob.png",
+		"images/tiledbackground4.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
